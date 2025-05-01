48032efbe11eaeb1d3bb193e9ba48960f06c06a8
David Santamaría <david@dasago.es>
2025-05-01 20:13:12 +0200

    work in ide_a_detail

--- scrapping/ide_a_detail.py
+++ scrapping/ide_a_detail.py
@@ -15,9 +15,9 @@ import undetected_chromedriver as uc
 from urllib.parse import urlparse, parse_qs
 
 ENLACE_INI = "https://www.idealista.com/inmueble/100000581/"
+ENLACE_INI = "https://www.idealista.com/inmueble/102146350/"
 
 browser = uc.Chrome()
-
 browser.get(ENLACE_INI)
 time.sleep(random.randint(4, 12))
 
@@ -36,11 +36,12 @@ except:
 last_height = browser.execute_script("return document.body.scrollHeight")
 while True:
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(random.randint(2, 4))
+    time.sleep(random.randint(1, 2))
     new_height = browser.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
     last_height = new_height
+time.sleep(random.randint(2, 4))
 
 
 
@@ -50,29 +51,75 @@ html = browser.page_source
 soup = bs(html, 'html.parser')
 
 full_detail = soup.find('section', {'class': 'detail-content-wrapper'})
-
 if full_detail:
     titulo = full_detail.find('span', {'class': 'main-info__title-main'}).text.strip()
     ubicacion = full_detail.find('span', {'class': 'main-info__title-minor'}).text.strip()
     precio  = float(full_detail.find('span', {'class': 'info-data-price'}).find('span',{'class':'txt-bold'}).text.strip().replace('.','').replace('€','').replace(',','.'))
+    etiquetas_html = full_detail.find('div', {'class': 'detail-info-tags'})
+    etiquetas_final =""
+    if etiquetas_html:
+        etiquetas = etiquetas_html.find_all('span', class_='tag')
+        etiquetas_final = " | ".join([etiqueta.text.strip() for etiqueta in etiquetas])
+    commentario = full_detail.find('div', {'class': 'comment'}).find('div', {'class': 'adCommentsLanguage'}).find('p').text.strip() 
+    comentario = "" if not commentario else commentario
 
-    """ idealista no carga la img hasta que el usuario hace scroll. Imagino que para ahorra costes de google maps."""
-    mapa = soup.find('div', {'class': 'static-map-container'})
-    map_img = mapa.find('img', id='sMap') if mapa else None
-    map_image_link = map_img.get('src') if map_img else ''
 
-    if map_image_link and len(map_image_link) > 10:
-        parsed_url = urlparse(map_image_link)
-        params = parse_qs(parsed_url.query)
-        center = params.get('center', [''])[0]
+""" Trabajo con el bloque de detalles """
+detalles = soup.find('section', {'class': 'details-box'}).find('div', {'class': 'details-property'})
 
+built_area = 0
+usable_area = 0
+bedrooms = 0
+bathrooms = 0
+garages = 0
+origin = ''
+status = ''
+builtin_wardrobes = 0
+orientation = ''
+heating = ''
+pmr_adapted = 0
+floor = ""
+lift = 0
+air_conditioning = 0
+pool = 0
+
+
+
+
+if detalles:
+    div_features_one = detalles.find('div', {'class': 'details-property-feature-one'})
+    lis = div_features_one.find_all('li')
+    for li in lis:
+        if 'habitaciones' in li.text.strip().lower():
+            habitaciones = li.find('span', {'class': 'value'}).text.strip()
    
-    print(titulo)
-    print(ubicacion)
-    print(precio)
-    print(map_image_link)
-    print(center)
 
-else:
-    print(f"No se ha podido carga el detalle del inmueble {ENLACE_INI}")
-    
+
+
+
+""" Trabajo con el mapa """
+mapa = soup.find('div', {'class': 'static-map-container'})
+map_img = mapa.find('img', id='sMap') if mapa else None
+map_image_link = map_img.get('src') if map_img else ''
+lat, lon = 0,0
+if map_image_link and len(map_image_link) > 10:
+    parsed_url = urlparse(map_image_link)
+    params = parse_qs(parsed_url.query)
+    center = params.get('center', [''])[0]
+    if "," in center:
+        lat, lon, *rest = center.split(",") #no creo que vengas más "," pero por si acaso
+lat, lon = float(lat), float(lon)
+
+            
+   
+
+
+
+""" Pruebas de lo que estamos haciendo """
+print(titulo)
+print(ubicacion)
+print(precio)
+print(lat, lon)
+print(etiquetas_final)
+print(commentario)
+
