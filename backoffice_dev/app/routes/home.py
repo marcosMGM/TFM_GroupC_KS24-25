030ba496ba41eb6c73e4ac3226fa67dda81dd750
David Santamaría <david@dasago.es>
2025-05-06 21:52:17 +0200

    maintenance page deploy

--- backoffice_dev/app/routes/home.py
+++ backoffice_dev/app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect, url_for, request, flash
+from flask import Blueprint, render_template, session, redirect, url_for, request, flash, g
 from app.utils.decorators import login_required
 
 home_controller = Blueprint('home_controller', __name__)
@@ -6,4 +6,11 @@ home_controller = Blueprint('home_controller', __name__)
 @home_controller.route("/")
 @login_required
 def index():
+    g.page_title = "Inicio"
     return render_template("pages/home/inicio.html")
+
+@home_controller.route("/mantenimiento")
+@home_controller.route("/maintenance")
+def maintenance():
+    g.page_title = "Mantenimiento"
+    return render_template("pages/maintenance/maintenance.html")
