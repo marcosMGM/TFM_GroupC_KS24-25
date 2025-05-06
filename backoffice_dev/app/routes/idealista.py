07de7f1eca8d701d61fb3815edf95f706881b117
David Santamaría <david@dasago.es>
2025-05-06 23:18:27 +0200

    working_DataTables.js integration

--- backoffice_dev/app/routes/idealista.py
+++ backoffice_dev/app/routes/idealista.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, g
 from app.utils.decorators import login_required
-# from app.models.idealista_model import get_user_by_login, update_last_access
+from app.models.idealista_model import get_datalist
 from werkzeug.security import check_password_hash
+from flask import jsonify
 
 idealista_controller = Blueprint('idealista_controller', __name__)
 
@@ -16,3 +17,26 @@ def index():
     return render_template("pages/idealista/list.html")
     # return redirect(url_for('home_controller.index'))
 
+
+
+""" 
+Esta es la petición que se hace desde el datatable al cargar la página:
+
+con esto debo controlar paginación, ordenación y búsqueda de los datos que se muestran en el datatable, 
+>serverside<
+
+127.0.0.1 - - [06/May/2025 23:14:31] "GET /idealista/datalist?draw=1&columns[0][data]=id&columns[0][name]=&columns[0][searchable]=true&columns[0][orderable]=true&columns[0][search][value]=&columns[0][search][regex]=false&columns[1][data]=titulo&columns[1][name]=&columns[1][searchable]=true&columns[1][orderable]=true&columns[1][search][value]=&columns[1][search][regex]=false&order[0][column]=0&order[0][dir]=asc&start=0&length=10&search[value]=&search[regex]=false&_=1746566070857 HTTP/1.1" 200 -
+
+
+ """
+@idealista_controller.route("/datalist")
+@login_required
+def datalist():
+    data = [
+        {"id": 1, "titulo": "Titulo 1"},
+        {"id": 2, "titulo": "Titulo 2"},
+        {"id": 3, "titulo": "Titulo 3"},
+        {"id": 4, "titulo": "Titulo 4"},
+        {"id": 5, "titulo": "Titulo 5"}
+    ]
+    return jsonify(data)
