45e9f7bd0e9caabb9ec67efd12c903d37bbb4be8
David Santamaría Gómez <david@dasago.es>
2025-05-14 20:38:03 +0200

    working_airbnb calendar import to sqlserver

---
+++ data_import/airbnb/sql_querys/01_CREATE TABLE bnb_calendar.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bnb_calendar (
+    property_id INT NOT NULL,
+    date DATE NOT NULL,
+    available BIT,
+    price DECIMAL(10,2),
+    minimum_nights INT,
+    maximum_nights INT,
+    PRIMARY KEY (property_id, date)
+);
