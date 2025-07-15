2117bfe32a739839d9bef318578beca9a33cb75c
David Santamaría <david@dasago.es>
2025-07-15 09:24:31 +0200

    changes

--- backoffice/app/routes/home.py
+++ backoffice/app/routes/home.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, session, redirect, url_for, reques
 from matplotlib import markers
 from app.utils.decorators import login_required
 from app.models.home_model import *
+from app.models.custom_model import get_parameters_by_key
 
 home_controller = Blueprint('home_controller', __name__)
 
@@ -9,9 +10,8 @@ home_controller = Blueprint('home_controller', __name__)
 @login_required
 def index():
     g.page_title = "Smart Summary"
-    return render_template("pages/home/inicio.html")
-
-
+    parameters = get_parameters_by_key()
+    return render_template("pages/home/inicio.html", parameters=parameters)
 
 @home_controller.route("/mantenimiento")
 @home_controller.route("/maintenance")
