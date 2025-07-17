910788a657911bb54ff9bfc9e36ac9addb5aeeef
David Santamaría <dsantamaria@samar.es>
2025-07-17 21:18:21 +0200

    deploy data explorer kpi & 1st chart

--- backoffice/app/models/statistics_model.py
+++ backoffice/app/models/statistics_model.py
@@ -4,25 +4,61 @@ import datetime
 from collections import defaultdict
 from app.models.idealista_model import get_percentile_roi
 from app.models.custom_model import get_parameters_by_key
+import json
+
 
 def get_home_kpi():
     db = DatabaseInterface()
     parameters = get_parameters_by_key()
     group_roi = get_percentile_roi() # dict con keys P33, P66, P90
 
-    card1 = db.getcountfromquery("SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0")
-    card2 = db.getcountfromquery(f"SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card01 = db.getcountfromquery("SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0")
+    card02 = db.getcountfromquery("SELECT DISTRITO FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 GROUP BY DISTRITO")
+    card03 = db.getallfromquery("SELECT ISNULL(AVG(PRICE), 0) AS precio_promedio FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0")
+    card03 = round(float(card03[0].get('precio_promedio', 0)), 2)
 
-    card5 = db.getallfromquery("SELECT ISNULL(SUM(PRICE) / NULLIF(SUM(BUILT_AREA), 0), 0) AS precio_medio FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0;")
+    houses_with_roi_gt_0 = db.getcountfromquery("SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 AND ROI > 0")
+    card04 = round((houses_with_roi_gt_0 / card01 * 100) if card01 > 0 else 0, 2)
+
+    card05 = db.getallfromquery("SELECT ISNULL(AVG(ROI), 0) AS promedio_roi FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0")
+    card05 = round(float(card05[0].get('promedio_roi', 0)), 2)
+    card06 = db.getallfromquery("SELECT ISNULL(AVG(NP), 0) AS promedio_np FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0")
+    card06 = round(float(card06[0].get('promedio_np', 0)), 2)
+
+    card11 = db.getcountfromquery(f"SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card12 = db.getcountfromquery(f"SELECT DISTRITO FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0  AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)} GROUP BY DISTRITO")
+    card13 = db.getallfromquery(f"SELECT ISNULL(AVG(PRICE), 0) AS precio_promedio FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0  AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card13 = round(float(card13[0].get('precio_promedio', 0)), 2)
+
+    houses_with_roi_gt_0 = db.getcountfromquery(f"SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 AND ROI > 0  AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card14 = round((houses_with_roi_gt_0 / card11 * 100) if card11 > 0 else 0, 2)
+
+    card15 = db.getallfromquery(f"SELECT ISNULL(AVG(ROI), 0) AS promedio_roi FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0  AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card15 = round(float(card15[0].get('promedio_roi', 0)), 2)
+    card16 = db.getallfromquery(f"SELECT ISNULL(AVG(NP), 0) AS promedio_np FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0  AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+    card16 = round(float(card16[0].get('promedio_np', 0)), 2)
+
+
+
+    # card2 = db.getcountfromquery(f"SELECT HOUSE_ID FROM HOUSES WHERE DISTRITO <> 'Not defined' AND PROCESSED=2 AND PRICE_PER_NIGHT IS NOT NULL AND PRICE_PER_NIGHT > 0 AND PRICE <= {parameters.get('MAX_INVESTMENT_BUDGET', dict).get('VALUE', 0)} AND ROI >= {parameters.get('INITIAL_MIN_ROI_DISPLAY_THRESOLD', dict).get('VALUE', -999)}")
+
+
 
     return {
-        'card1': card1,
-        'card2': card2, 
-        'card3': 0,
-        'card4': 0,
-        'card5': round(float(card5[0].get('precio_medio', 0)),2),
+        'card01': card01,
+        'card02': card02, 
+        'card03': card03,
+        'card04': card04,
+        'card05': card05,
+        'card06': card06,
+        'card11': card11,
+        'card12': card12,
+        'card13': card13,
+        'card14': card14,
+        'card15': card15,
+        'card16': card16,
+        
     }
-
 
 def get_houses_by_distrito():
     db = DatabaseInterface()
@@ -56,8 +92,41 @@ def get_houses_by_distrito():
             WHEN ROI > {group_roi.get('P90')} THEN 'Excelente'
     END
     ORDER BY DISTRITO ASC;"""
-    data = db.getallfromquery(sql)
-    return data
+    casas = db.getallfromquery(sql)
+
+    roi_categories = ['No Rentable', 'Baja', 'Media', 'Alta', 'Excelente']
+    color_map = {
+        'No Rentable': '#FF5E40',
+        'Baja': '#48443D',
+        'Media': '#EBC33F',
+        'Alta': '#4FC9DA',
+        'Excelente': '#AECC34'
+    }
+
+    districts, seen = [], set()
+    for item in casas:
+        d = item['DISTRITO']
+        if d not in seen:
+            districts.append(d)
+            seen.add(d)
+
+    data_by_district = {d: {c: 0 for c in roi_categories} for d in districts}
+    for item in casas:
+        data_by_district[item['DISTRITO']][item['ROI_GROUP']] = item['PROPIEDADES']
+
+    series = [
+        {'name': cat, 'data': [data_by_district[d][cat] for d in districts]}
+        for cat in roi_categories
+    ]
+
+    chart_dict = {
+        'series': series,
+        'categories': districts,
+        'colors': [color_map[c] for c in roi_categories]
+    }
+
+    chart_data_json = json.dumps(chart_dict, ensure_ascii=False)
+    return chart_data_json
 
 
 def get_map_markers():
